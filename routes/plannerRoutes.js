5642d41c5a86343d381274baf0243dbd66402164
phi nguyen <phinguyen712@gmail.com>
2016-09-15 22:42:25 +0000

    fixed the previous day and next day button to shuffle between days

--- routes/plannerRoutes.js
+++ routes/plannerRoutes.js
@@ -74,7 +74,6 @@ router.post("/planner/journey/show", function(req,res){
          foundUser.currentJourney.id = journeyId;
          foundUser.currentJourney.name = journeyName;
          foundUser.save();
-          console.log(foundUser);
           foundUser.journeys.forEach(function(journey){
               if(journey._id == req.body.journeyId){
                    populateUsersData(req,res,journey.days[0].journeySchedule,
@@ -89,6 +88,25 @@ router.post("/planner/journey/show", function(req,res){
     });
 }); 
 
+
+router.post("/planner/days", function(req,res){
+  journeys.findById(req.body.journeyId,function(err,foundJourney){
+    if(err){
+        console.log(err);
+    }else{
+        var day = foundJourney.days[req.body.day];
+        if(!foundJourney.days[req.body.day]){
+            day = "";
+        }
+        populateUsersData(req,res,day.journeySchedule,
+            function(tempArr){
+                res.json(tempArr);
+            }
+        );
+    }
+    });
+});
+
 router.post("/planner/toDo/new",function(req,res){
     var dayIndex = parseInt(req.body.day)- 1;//current day of the sortpanel 
      //add favorites to journey database in journeys.days[x].journeySchedule 
@@ -96,14 +114,12 @@ router.post("/planner/toDo/new",function(req,res){
         if(err){
             console.log(err);
         }else{
-              if(!foundJourney.days[dayIndex].journeySchedule){
-
+              if(foundJourney.days[dayIndex] == ""){
                 foundJourney.days[dayIndex] = {journeySchedule:[req.body.id]};
               }else{
                 foundJourney.days[dayIndex].journeySchedule.push(req.body.id);
               } 
                 foundJourney.save();
-                
                 populateUsersData(req,res,foundJourney.days[dayIndex].journeySchedule,
                     function(tempArr){
                         res.json(tempArr);
@@ -120,12 +136,9 @@ router.put("/planner/schedule/edit",function(req,res){
             console.log(err);
         }else{
             var dayIndex = parseInt(req.body.day)- 1;
-            console.log(dayIndex);
-            console.log(foundJourney);
             foundJourney.days[dayIndex].journeySchedule = req.body.id;
             var scheduleList = foundJourney.days[dayIndex].journeySchedule;
             foundJourney.save();
-            console.log(scheduleList);
             populateUsersData(req,res,scheduleList,function(tempArr){
                     res.json(tempArr);
                     });
